de846b8582c8630c0f62a09ea35986942301e8f9
m1orez <pauline@walhout-hia.nl>
2026-01-28 17:01:33 +0100

    Email.js added

--- script.js
+++ script.js
@@ -9,9 +9,18 @@ document.querySelectorAll('nav a').forEach(link => {
 const form = document.getElementById("contactForm");
 const msg = document.getElementById("formMsg");
 
-form.addEventListener("submit", e => {
+form.addEventListener("submit", function(e) {
   e.preventDefault();
-  msg.textContent = "Message sent successfully!";
-  msg.style.color = "#ff2b87";
-  form.reset();
+
+  emailjs.sendForm('MIOREZ_PORTFOLIO', 'template_sl1yk5i', this)
+    .then(() => {
+      msg.textContent = "Message sent successfully!";
+      msg.style.color = "#ff2b87";
+      form.reset();
+    })
+    .catch((error) => {
+      msg.textContent = "Oops! Something went wrong.";
+      msg.style.color = "red";
+      console.error('EmailJS Error:', error);
+    });
 });
